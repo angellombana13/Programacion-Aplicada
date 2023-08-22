40a6e707aa88d6a2a3ebce37e801bffbd4c7fc14
Angel <70928745+angellombana13@users.noreply.github.com>
2023-08-22 13:29:55 -0500

    Add files via upload

---
+++ numeros primos.py
@@ -0,0 +1,17 @@
+while True:
+
+    num = int(input("Enter a positive natural number: "))
+    print("Number: ", num)
+
+    n = num%2
+
+    if(n==1):
+        print("Number ", num, "it's prime")
+
+    else:
+        print("Number", num, "it's not prime")
+
+    question = str(input("Would you like to stop?"))
+    if (question =='Yes')
+        print("Program has finished")
+    if (question == 'No')
