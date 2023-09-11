7d21328038fc8f5e3956cd4d7e77c344a8115f02
Angel <70928745+angellombana13@users.noreply.github.com>
2023-09-11 18:54:00 -0500

    ThirdProgram.py

---
+++ PrimerosProgramas/ThirdProgram.py
@@ -0,0 +1,20 @@
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "Pancakes"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "Ajiaco"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = "Baby beef"
+
+# Printing out dinner
+print("Dinner:")
+print(meal)
